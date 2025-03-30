e47ddb27fe4c32c9a3fdc87e3d42df56ab115e57
Peyman <hassani.peyman627@gmail.com>
2025-03-30 17:48:42 +0330

    Add subtract handler

--- handlers.go
+++ handlers.go
@@ -33,7 +33,30 @@ func handleAdd(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleSubtract(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Number1 int `json:"number1"`
+		Number2 int `json:"number2"`
+	}
 
+	decoder := json.NewDecoder(r.Body)
+	defer r.Body.Close()
+
+	var params parameters
+	err := decoder.Decode(&params)
+	if err != nil {
+		responseWithJsonError(w, http.StatusBadRequest, "invalid input")
+		return
+	}
+
+	type response struct {
+		Result int `json:"result"`
+	}
+
+	resp := response{
+		Result: params.Number1 - params.Number2,
+	}
+
+	responseWithJson(w, http.StatusOK, resp)
 }
 
 func handleMultiply(w http.ResponseWriter, r *http.Request) {
