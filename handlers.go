cce4400db79f2999de3b03885d2c3497974bf298
Peyman <hassani.peyman627@gmail.com>
2025-03-30 17:50:39 +0330

    Add multiply handler

--- handlers.go
+++ handlers.go
@@ -60,7 +60,30 @@ func handleSubtract(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleMultiply(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Number1 int `json:"number1"`
+		Number2 int `json:"number2"`
+	}
 
+	decoder := json.NewDecoder(r.Body)
+	defer r.Body.Close()
+
+	var params parameters
+	err := decoder.Decode(&params)
+	if err != nil {
+		responseWithJsonError(w, http.StatusBadRequest, "invalid input")
+		return
+	}
+
+	type response struct {
+		Result int `json:"result"`
+	}
+
+	resp := response{
+		Result: params.Number1 * params.Number2,
+	}
+
+	responseWithJson(w, http.StatusOK, resp)
 }
 
 func handleDivide(w http.ResponseWriter, r *http.Request) {
