489f04ed386f0b68a1f80c24f26922a6e69d65f3
Peyman <hassani.peyman627@gmail.com>
2025-03-30 15:40:04 +0330

    Update add handler

--- handlers.go
+++ handlers.go
@@ -1,10 +1,42 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func handleAdd(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(201)
-	w.Write([]byte("add numbers"))
+	type parameters struct {
+		Number1 int `json:"number1"`
+		Number2 int `json:"number2"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+
+	var params parameters
+	err := decoder.Decode(&params)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("invalid input"))
+		return
+	}
+
+	type response struct {
+		Result int `json:"result"`
+	}
+
+	resp := response{
+		Result: params.Number1 + params.Number2,
+	}
+
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResp)
 }
 
 func handleSubtract(w http.ResponseWriter, r *http.Request) {
